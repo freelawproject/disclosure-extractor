2f6a02f12ff0dc08f53b4c875308d6f03cad4114
vvilliam <wpalin@gmail.com>
2020-08-24 14:12:58 -0400

    feat(calculate.py): Update networth calculators

--- disclosure_extractor/calculate.py
+++ disclosure_extractor/calculate.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-def estimate_net_worth(extracted_data):
+def estimate_investment_net_worth(extracted_data):
     """Currenlty only using investment table to calculate net worth
 
     """
@@ -20,13 +20,47 @@ def estimate_net_worth(extracted_data):
             1000000000,
         ],  # This is inaccurate as their is no upper bound
     }
-    low = []
-    high = []
-    for investments in extracted_data["investments_and_trusts"]:
-        if investments["gross_value"]:
-            low.append(money[investments["gross_value"]][0])
-            high.append(money[investments["gross_value"]][1])
-        if investments["transaction_value_code"]:
-            low.append(money[investments["transaction_value_code"]][0])
-            high.append(money[investments["transaction_value_code"]][1])
-    return (sum(low), sum(high))
+    investments = extracted_data["Investments and Trusts"]["content"]
+    gross_values = [
+        money[x["text"]]
+        for x in investments
+        if "Value Code" in x["field"] and x["text"] in money.keys()
+    ]
+    low = sum(x[0] for x in gross_values)
+    high = sum(x[1] for x in gross_values)
+    return (low, high)
+
+
+def income_gains(extracted_data):
+    """Currenlty only using investment table to calculate net worth
+
+    """
+    money = {
+        "A": [1, 1000],
+        "B": [1001, 2500],
+        "C": [2501, 5000],
+        "D": [5001, 15000],
+        "E": [15001, 50000],
+        "F": [50001, 100000],
+        "G": [100001, 1000000],
+        "H1": [1000001, 5000000],
+        "H2": [
+            5000000,
+            1000000000,
+        ],  # This is inaccurate as their is no upper bound
+    }
+    investments = extracted_data["Investments and Trusts"]["content"]
+    gain_1 = [
+        money[x["text"]]
+        for x in investments
+        if "D Gain Code" == x["field"] and x["text"] in money.keys()
+    ]
+    gain_2 = [
+        money[x["text"]]
+        for x in investments
+        if "B Amount Code" == x["field"] and x["text"] in money.keys()
+    ]
+    low = sum(x[0] for x in gain_1 + gain_2)
+    high = sum(x[1] for x in gain_1 + gain_2)
+
+    return (low, high)
