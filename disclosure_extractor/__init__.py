cc7c6394187aa0f6d8e9088725f13a4b12ae47e0
vvilliam <wpalin@gmail.com>
2020-09-01 09:15:20 -0400

    to remove

--- disclosure_extractor/__init__.py
+++ disclosure_extractor/__init__.py
@@ -163,3 +163,12 @@ def process_financial_document(
 
     results["success"] = True
     return results
+
+try:
+    import importlib.resources as importlib_resources
+except ImportError:
+    # In PY<3.7 fall-back to backported `importlib_resources`.
+    import importlib_resources
+
+def get_data():
+    return importlib_resources.read_text(__name__, 'extractor_template.json')
