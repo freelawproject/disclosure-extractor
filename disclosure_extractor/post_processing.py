d5faa92d32a65be767125d2bfe42c7dc335b4fa7
vvilliam <wpalin@gmail.com>
2020-12-08 14:20:39 -0500

    style(post_processing): Black

--- disclosure_extractor/post_processing.py
+++ disclosure_extractor/post_processing.py
@@ -7,28 +7,27 @@ def _fine_tune_results(results: dict) -> Dict:
     :param results: Raw data extracted by OCR
     :return: Refined data
     """
-
-    for k, v in results['sections'].items():
-        del v['empty']
-        del v['order']
+    for k, v in results["sections"].items():
+        del v["empty"]
+        del v["order"]
         rows = []
 
-        for k1, v1 in v['rows'].items():
+        for k1, v1 in v["rows"].items():
             single_row = []
             for k2, v2 in v1.items():
-                single_row.append(v2['text'])
+                single_row.append(v2["text"])
             if len("".join(single_row)) > 3:
                 rows.append(v1)
-        v['rows'] = rows
+        v["rows"] = rows
 
     # Cleanup Investments and Trusts
-    for row in results['sections']['Investments and Trusts']['rows']:
-        print(row['B2'])
-        if "rest" in row['B2']['text']:
-            row['B2']['text'] = "Interest"
-        if "Dist" in row['B2']['text']:
-            row['B2']['text'] = "Distribution"
-        if "idend" in row['B2']['text']:
-            row['B2']['text'] = "Dividend"
+    for row in results["sections"]["Investments and Trusts"]["rows"]:
+        print(row["B2"])
+        if "rest" in row["B2"]["text"]:
+            row["B2"]["text"] = "Interest"
+        if "Dist" in row["B2"]["text"]:
+            row["B2"]["text"] = "Distribution"
+        if "idend" in row["B2"]["text"]:
+            row["B2"]["text"] = "Dividend"
 
     return results
