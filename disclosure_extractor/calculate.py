45f0baefd1f70ff14b3aa8a9c713c2c93e3a90c5
vvilliam <wpalin@gmail.com>
2021-03-17 12:13:33 -0400

    feat(calculate): Add estimate_investment_net_worth_JEF

--- disclosure_extractor/calculate.py
+++ disclosure_extractor/calculate.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import re
+from typing import Dict
 
 
 class color:
@@ -92,6 +93,81 @@ def estimate_investment_net_worth(results):
                 salary = v["Income"]["text"].replace(",", "").strip("$")
                 if not re.match(r"^-?\d+(?:\.\d+)?$", salary) is None:
                     salaries.append(float(salary))
+
+        cd["salary_income"] = sum(salaries)
+    except:
+        cd["salary_income"] = 0
+    return cd
+
+
+def estimate_investment_net_worth_JEF(results: Dict) -> Dict:
+    """Currently only using investment table to calculate net worth"""
+    key_codes = {
+        "A": [1, 1000],
+        "B": [1001, 2500],
+        "C": [2501, 5000],
+        "D": [5001, 15000],
+        "E": [15001, 50000],
+        "F": [50001, 100000],
+        "G": [100001, 1000000],
+        "H1": [1000001, 5000000],
+        "H2": [  # This is inaccurate as their is no upper bound
+            5000001,
+            1000000000,
+        ],
+        "J": [1, 15000],
+        "K": [15001, 50000],
+        "L": [50001, 100000],
+        "M": [100001, 250000],
+        "N": [250001, 500000],
+        "O": [500001, 1000000],
+        "P1": [1000001, 5000000],
+        "P2": [5000001, 25000000],
+        "P3": [25000001, 50000000],
+        "P4": [  # This is inaccurate as their is no upper bound
+            50000001,
+            1000000000,
+        ],
+    }
+    gross_values = []
+    for v in results["sections"]["Investments and Trusts"]["rows"]:
+        if v["C1"]["text"] != "" and v["C1"]["text"] != "•":
+            gross_values.append(key_codes[v["C1"]["text"]])
+        if v["D3"]["text"] != "" and v["D3"]["text"] != "•":
+            gross_values.append(key_codes[v["D3"]["text"]])
+
+    low = sum(x[0] for x in gross_values)
+    high = sum(x[1] for x in gross_values)
+    cd = {}
+    cd["investment_net_worth"] = (low, high)
+
+    net_change = []
+    for v in results["sections"]["Investments and Trusts"]["rows"]:
+        B1, D4 = v["B1"]["text"], v["D4"]["text"]
+        for code in [B1, D4]:
+            if code != "" and code != "•":
+                net_change.append(key_codes[code])
+
+    low = sum(x[0] for x in net_change)
+    high = sum(x[1] for x in net_change)
+    cd["income_gains"] = (low, high)
+
+    liabilities_total = []
+    try:
+        for v in results["sections"]["Liabilities"]["rows"]:
+            liabilities_total.append(key_codes[v["Value Code"]["text"]])
+
+        low = sum(x[0] for x in liabilities_total)
+        high = sum(x[1] for x in liabilities_total)
+        cd["liabilities"] = (low, high)
+    except:
+        cd["liabilities"] = (0, 0)
+    try:
+        salaries = []
+        for v in results["sections"]["Non-Investment Income"]["rows"]:
+            salary = v["Income"]["text"].replace(",", "").strip("$")
+            if not re.match(r"^-?\d+(?:\.\d+)?$", salary) is None:
+                salaries.append(float(salary))
 
         cd["salary_income"] = sum(salaries)
     except:
